d8c10a1c8ffbb7ea2250d16e0f586173487636d7
Minggang Wang <minggangw@gmail.com>
2014-02-08 11:31:37 +0800

    Using divide-conquer algorithm to count 1

--- offer/10_NumberOf1InBinary/NumberOf1InBinary.cpp
+++ offer/10_NumberOf1InBinary/NumberOf1InBinary.cpp
@@ -7,6 +7,29 @@
 #include <cstdio>
 #include <cstdlib>
 
+typedef unsigned int uint32;  //assume this gives 64-bits
+const uint32 m1  = 0x55555555; //binary: 0101...
+const uint32 m2  = 0x33333333; //binary: 00110011..
+const uint32 m4  = 0x0f0f0f0f; //binary:  4 zeros,  4 ones ...
+const uint32 m8  = 0x00ff00ff; //binary:  8 zeros,  8 ones ...
+const uint32 m16 = 0x0000ffff; //binary: 16 zeros, 16 ones ...
+// const uint32 m32 = 0x00000000ffffffff; //binary: 32 zeros, 32 ones ...
+// const uint32 hff = 0xffffffffffffffff; //binary: all ones
+// const uint32 h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
+ 
+//This is a naive implementation, shown for comparison,
+//and to help in understanding the better functions.
+//It uses 24 arithmetic operations (shift, add, and).
+int NumberOf1_Solution3(int  x) {
+    x = (x & m1 ) + ((x >>  1) & m1 ); //put count of each  2 bits into those  2 bits 
+    x = (x & m2 ) + ((x >>  2) & m2 ); //put count of each  4 bits into those  4 bits 
+    x = (x & m4 ) + ((x >>  4) & m4 ); //put count of each  8 bits into those  8 bits 
+    x = (x & m8 ) + ((x >>  8) & m8 ); //put count of each 16 bits into those 16 bits 
+    x = (x & m16) + ((x >> 16) & m16); //put count of each 32 bits into those 32 bits 
+    // x = (x & m32) + ((x >> 32) & m32); //put count of each 64 bits into those 64 bits 
+    return x;
+}
+
 int NumberOf1_Solution1(int n)
 {
     int count = 0;
@@ -48,6 +71,12 @@ void Test(int number, unsigned int expected)
         printf("Solution2: Test for %p passed.\n", number);
     else
         printf("Solution2: Test for %p failed.\n", number);
+
+    actual = NumberOf1_Solution3(number);
+    if(actual == expected)
+        printf("Solution3: Test for %p passed.\n", number);
+    else
+        printf("Solution3: Test for %p failed.\n", number);
 
     printf("\n");
 }
