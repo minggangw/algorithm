9d7937c3b47dea909f2078c5a6af01d2516adb38
Minggang Wang <minggangw@gmail.com>
2014-03-03 14:20:13 +0800

    Follow c++ coding convention

--- DP_Problem/LongestIncreasingSubsequence/LongestIncreasingSubsequence.cpp
+++ DP_Problem/LongestIncreasingSubsequence/LongestIncreasingSubsequence.cpp
@@ -1,61 +1,60 @@
 #include <iostream>
+
 using namespace std;
- 
+
 //动态规划法求最长递增子序列 LIS
- 
+
 int dp[101]; /* 设数组长度不超过100，dp[i]记录到[0,i]数组的LIS */
 int lis;    /* LIS 长度 */
- 
+
 int LIS(int * arr, int size)
 {
-    for(int i = 0; i < size; ++i)
-    {
+    for(int i = 0; i < size; ++i) {
         dp[i] = 1;
-        for(int j = 0; j < i; ++j)
-        {
-            if(arr[i] > arr[j] && dp[i] < dp[j] + 1)
-            {
+
+        for(int j = 0; j < i; ++j) {
+            if(arr[i] > arr[j] && dp[i] < dp[j] + 1) {
                 dp[i] = dp[j] + 1;
-                if(dp[i] > lis)
-                {
+                if(dp[i] > lis) {
                     lis = dp[i];
                 }
             }
         }
     }
+
     return lis;
 }
- 
+
 /* 输出LIS */
 void outputLIS(int * arr, int index)
 {
     bool isLIS = 0;
+
     if(index < 0 || lis == 0)
-    {
         return;
-    }
-    if(dp[index] == lis)
-    {
+
+    if(dp[index] == lis) {
         --lis;
         isLIS = 1;
     }
- 
-    outputLIS(arr,--index);
- 
+
+    outputLIS(arr, --index);
+
     if(isLIS)
-    {
         printf("%d ",arr[index+1]);
-    }
 }
- 
-void main()
+
+int main()
 {
-    int arr[] = {1,-1,2,-3,4,-5,6,-7};
- 
+    // int arr[] = {1,-1,2,-3,4,-5,6,-7};
+    int arr[] = {1, 5, 8, 2, 3, 4};
+
     /* 输出LIS长度； sizeof 计算数组长度 */
     printf("%d\n",LIS(arr,sizeof(arr)/sizeof(int)));
- 
+
     /* 输出LIS */
     outputLIS(arr,sizeof(arr)/sizeof(int) - 1);
     printf("\n");
+
+    return 0;
 }
